bd702099e0fddec6a24c9309afa2231eea32ad9f
deekshithm0 <deekshithm0@gmail.com>
2024-10-22 17:58:20 +0530

    app and clients updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 
 import './App.css'
+import Clients from './components/Clients'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeLayout from './components/HomeLayout'
 
 function App() {
   
 
   return (
     <>
+      <Header/>
       <h1>Faiza Einab</h1>
+      <HomeLayout/>
+      <Clients/>
+      <Footer/>
     </>
   )
 }
