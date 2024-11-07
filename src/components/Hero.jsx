bd8832d3fef9f5525324a7be85b0408114bef800
Adhil-tp <mohamedadhiltp1944@gmail.com>
2024-11-07 13:50:31 +0530

    boiler plate created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
+import React from "react"
+import Grapes from "../assets/elements/grapes.png"
 
 const Hero = () => {
   return (
-    <div>
-      <h1>This is hero section</h1>
-    </div>
+    <>
+      <div className="h-[2vh] pt-20">
+        <h1>This is hero section</h1>
+      </div>
+      <img src={Grapes} alt="" className="fixed w-1/3  right-6 top-[-200px]" />
+    </>
   )
 }
 
