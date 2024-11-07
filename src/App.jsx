801ee7c23ca5ea9fcd12c06e14b223e68670c752
deekshithm0 <deekshithm0@gmail.com>
2024-11-07 14:20:44 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Clients from './components/Clients'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 
