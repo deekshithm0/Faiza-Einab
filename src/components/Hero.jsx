5b40b44597739bc357340a0f8d8113ea643d81b2
deekshithm0 <deekshithm0@gmail.com>
2024-11-29 21:27:31 +0530

    whatsapp window added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -58,7 +58,7 @@ const Hero = () => {
         <div className="h-screen z-10 bg-cover bg-center flex flex-row items-center justify-center gap-80 " >
           <div className="flex flex-col pb-[170px] gap-4">
             <h1 className="font-italiana text-[6rem] leading-tight text-[#582E7D]">Simply <br /> Pure, Simply <br /> Delecious!</h1>
-            <button className="w-40 h-12 ml-7 rounded-3xl bg-[#582E7D] text-white text-center" >Get in touch</button>
+            <button className="w-40 h-12 ml-7 rounded-3xl bg-[#582E7D] text-white text-center" onClick={openWhatsapp} >Get in touch</button>
           </div>
           <Bottle />
         </div>
